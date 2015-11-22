4cd95456df69ffb9a7623c5944b041b81f79a4b7
capouch <brianc@palaver.net>
2015-11-22 03:28:09 -0500

    Remove old cruft

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Set up server, hook on processors for sockets and files
 var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
-var fs = require('fs');
 
 app.listen(8000);
 
